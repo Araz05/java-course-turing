acb5dc3341a7329109acf92356deedb8e023fc96
Araz05 <valishovaraz@gmail.com>
2024-09-22 19:49:25 +0400

    initial commit

--- src/main/java/az/edu/turing/App.java
+++ src/main/java/az/edu/turing/App.java
@@ -4,8 +4,7 @@ package az.edu.turing;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
